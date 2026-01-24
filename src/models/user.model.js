3417d6baaab9348adbaee28c034a8f44d8945e8d
Parth Patel <creatinganewway@gmail.com>
2026-01-24 17:12:47 +0530

    user and video model are created

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,6 +58,32 @@ userSchema.pre("save" , async function (next){
 userSchema.methods.isPasswordCorrect = async function(password){
    return await bcrypt.compare(password/*enterred by user*/, this.password/*hashed pass jo db me he*/)  //return boolean value
 }
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User" , userSchema)
 
 
