5d16781ccd5585fb35073059d640232134ba318b
Parth Patel <creatinganewway@gmail.com>
2026-01-26 15:03:12 +0530

    started making controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req , res)=>{
-    res.status(200).json({
-        success: true,
-        message: "Parth-aur-code"
-    })
+    // res.status(200).json({
+    //     success: true,
+    //     message: "Parth-aur-code"
+    // })
+
+    //get user deatails like username , email , password etc from frontend
+    //validation lagana padega like - not empty
+    //check if user already exist : through username , email
+    //check for images , check for avatar
+    //upload them to cloudinary , avatar check on cloudinary
+
 })
 
 
